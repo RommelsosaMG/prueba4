7d060b044071bc13f8046a8c12ead4da1aee1573
RommelsosaMG <122042187+RommelsosaMG@users.noreply.github.com>
2023-01-06 17:36:42 -0500

    prueba de consulta

--- block de notas.sql
+++ block de notas.sql
@@ -4,3 +4,16 @@ SELECT * FROM LIFE_COVER where NBRANCH=73
 --mejoras en el codigo
 
 --se realizan cambios en el uso de git
+
+-- nueva mejora de la mejora.
+
+SELECT * FROM LIFE_COVER WHERE NBRANCH = 73 AND NPRODUCT = 1 AND NMODULEC = 22 AND NCOVER = 6;
+
+--nueva mejora
+
+
+
+
+
+
+
