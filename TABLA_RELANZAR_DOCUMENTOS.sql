f0df6c9959b0f08752dadde288fb70b08a4bcb70
RommelsosaMG <122042187+RommelsosaMG@users.noreply.github.com>
2023-05-26 11:54:02 -0500

    TABLA PARA RELANZAR DOCUMENTOS

--- TABLA_RELANZAR_DOCUMENTOS.sql
+++ TABLA_RELANZAR_DOCUMENTOS.sql
@@ -1,3 +1,6 @@
 ------Relanzar documentos------------------------------------
 ----- cambiar en el campo NENV_PRINT  el estado 7 al 0 , para relanzar los documentos // EN PRODUCCION EL ESTADO ES 1
 select * from TBL_TRX_CARGA_DEFINITIVA WHERE NPOLICY='6000009787' AND NIDHEADERPROC=36048 FOR UPDATE;
+
+update TBL_TRX_CARGA_DEFINITIVA set NENV_PRINT=0 where NPOLICY='63834'and nbranch=61
+
